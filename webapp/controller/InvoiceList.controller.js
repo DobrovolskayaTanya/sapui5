c73e20ce36d8148d4b91a8f6b32705d50f016e25
Tatsiana Dabravolskaya <dobrovolskaya1.18@gmail.com>
2020-10-13 10:15:46 +0000

    with Routing

--- webapp/controller/InvoiceList.controller.js
+++ webapp/controller/InvoiceList.controller.js
@@ -4,31 +4,36 @@ sap.ui.define([
 	"../model/formatter",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator"
-], function (Controller, JSONModel,formatter,Filter,FilterOperator ) {
+], function (Controller, JSONModel, formatter, Filter, FilterOperator) {
 	"use strict";
 	return Controller.extend("sap.ui.demo.walkthrough.controller.InvoiceList", {
-		formatter:formatter,
+		formatter: formatter,
 		onInit: function () {
 			var oViewModel = new JSONModel({
 				currency: "EUR"
 			});
-			this.getView().setModel( oViewModel, "view");
+			this.getView().setModel(oViewModel, "view");
 		},
-		onFilterInvoices: function(oEvent){
+		onFilterInvoices: function (oEvent) {
 			//build filter array
-			var aFilter =[];
+			var aFilter = [];
 			var sQuery = oEvent.getParameter("query");
-			if(sQuery){
-				aFilter.push(new Filter("ProductName", FilterOperator.Contains,sQuery));
-	
+			if (sQuery) {
+				aFilter.push(new Filter("ProductName", FilterOperator.Contains, sQuery));
+
 			}
 			//filter binding
 			var oList = this.byId("invoiceList");
-			var oBinding =oList.getBinding("items");
+			var oBinding = oList.getBinding("items");
 			oBinding.filter(aFilter);
-			
-		
+		},
+		onPress: function (oEvent) {
+			var oItem = oEvent.getSource();
+			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+			oRouter.navTo("detail",{
+				invoicePath:window.encodeURIComponent(oItem.getBindingContext("invoice").getPath().substr(1))
+			});
 		}
 	});
-	
+
 });
