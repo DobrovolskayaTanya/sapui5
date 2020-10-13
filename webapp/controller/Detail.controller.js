42cbec9584d7a665e33fa0d11be09ed6d694e0cd
Tatsiana Dabravolskaya <dobrovolskaya1.18@gmail.com>
2020-10-13 12:27:58 +0000

    with custom Control

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/UIComponent",
-	"sap/ui/core/routing/History"
-], function (Controller, UIComponent, History) {
+	"sap/ui/core/routing/History",
+	"sap/m/MessageToast"
+], function (Controller, UIComponent, History, MessageToast) {
 	"use strict";
 	return Controller.extend("sap.ui.demo.walkthrough.controller.Detail", {
 		onInit: function () {
@@ -10,22 +11,30 @@ sap.ui.define([
 			oRouter.getRoute("detail").attachPatternMatched(this._onObjectMatched, this);
 		},
 		_onObjectMatched: function (oEvent) {
+			this.byId("rating").reset();
 			this.getView().bindElement({
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").invoicePath),
 				model: "invoice"
 			});
-			},
-			onNavBack: function(){
-				var oHistory = History.getInstance();
-				var SPreviousHash = oHistory.getPreviousHash();
-				
-				if(SPreviousHash !== undefined){
-					window.history.go(-1);
-				}else{
-					var oRouter = UIComponent.getRouterFor(this) ;
-					oRouter.navTi("overview",{},true);
-				}
+		},
+		onNavBack: function () {
+			var oHistory = History.getInstance();
+			var SPreviousHash = oHistory.getPreviousHash();
+
+			if (SPreviousHash !== undefined) {
+				window.history.go(-1);
+			} else {
+				var oRouter = UIComponent.getRouterFor(this);
+				oRouter.navTi("overview", {}, true);
 			}
-		
+		},
+		onRatingChange: function (oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+			
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
+  
+		}
+
 	});
 });
